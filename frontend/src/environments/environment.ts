ee1f4fbbcad9523ce3b60eb10cf2e58da419d1c2
LyHocVu <121430765+NightoSlep@users.noreply.github.com>
2025-06-28 22:22:44 +0700

    Delete frontend/src/environments directory

--- frontend/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  googleClientId: '195135094824-vb1jjmcvm7vfvi4oavidbjnrfeargq10.apps.googleusercontent.com',
-  facebookAppId: '675946521577798',
-  apiUrl: '/api'
-};
